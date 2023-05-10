bb9978ffa67a89c100583cfd673922b714efca81
David Min <davidmin12@gmail.com>
2023-05-10 11:52:53 -0700

    Modified main.go import path

--- cmd/zgrab2/main.go
+++ cmd/zgrab2/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/zmap/zgrab2/bin"
-	_ "github.com/zmap/zgrab2/modules"
+	"github.com/dmin12/zgrab_tls1.3/bin"
+	_ "github.com/dmin12/zgrab_tls1.3/modules"
 )
 
 // main wraps the "true" main, bin.ZGrab2Main(), after importing all scan
